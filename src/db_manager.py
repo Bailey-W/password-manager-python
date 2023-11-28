dd33d96d2505e0613a6dcf94c18b4941ece49fb1
NerosJourney <sandman4264@gmail.com>
2023-11-27 23:54:20 -0500

    Created database connection

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,10 +1,28 @@
-# Creates a new SQLite Database with the expected schema
-def create_database():
-    pass
+import sqlite3
+import os
+import logger
 
-# Checks if a SQLite Database already exists
-def check_for_db() -> bool:
-    pass
+def setup_db(connection):
+    logger.log_event("Setting up database for first run")
+    cur = connection.cursor()
+    cur.execute("CREATE TABLE entries(username, url, password)")
+    logger.log_event("Database setup complete")
+
+# Connects to the SQLite database. Creates the database if it does not exist.
+# Returns: a database connection object
+def connect_to_db():
+    db_location = os.path.dirname(os.path.abspath(__file__)) + "/../data/data.db"
+    
+    is_new_db = not os.path.isfile(db_location)
+
+    logger.log_event("Connecting to database")
+    con = sqlite3.connect(db_location)
+    logger.log_event("Successfully connected to database")
+
+    if is_new_db:
+        setup_db(con)
+
+    return con
 
 # Finds the password(s) matching the query
 # Expects: username, the username to find
@@ -18,3 +36,5 @@ def get_encrypted_passwords(username: str, url: str) -> list:
 # Returns: A list of all entries containing the query in either username or url fields
 def find_entries_containing(query: str) -> list:
     pass
+
+connect_to_db()
