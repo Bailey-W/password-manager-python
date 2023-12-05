1d2a8dc97ab8f87786b3e246304ecba45f0edb5d
NerosJourney <sandman4264@gmail.com>
2023-12-05 12:00:15 -0500

    Add generation and verification of passwords

--- src/keygen.py
+++ src/keygen.py
@@ -26,8 +26,45 @@ def gen_or_get_salt():
     return salt
 
 def generate_key_from_password(password):
-    logger.log_event("Generating master key from master password")
+    logger.log_event("Generating master key from master password", __name__)
+    password = bytes(password, encoding='utf-8')
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=gen_or_get_salt(),
+        iterations=480000,
+    )
+    key = kdf.derive(password)
+    return key
     
 
+def verify_password(password):
+    password = bytes(password, encoding='UTF-8')
+    logger.log_event('Verifying password...', __name__)
+    verification_location = os.path.dirname(os.path.abspath(__file__)) + "/../keys/verify.key"
+
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(password)
+    hashed_password = digest.finalize()
+
+    if not os.path.isfile(verification_location):
+        # Old password not found
+        logger.log_event('Previous password missing. Saving this as new password.', __name__)
+        f = open(verification_location, 'wb')
+        f.write(hashed_password)
+        return True
+    else:
+        logger.log_event('Found previous password. Verfying.', __name__)
+        f = open(verification_location, 'rb')
+        old_hash = f.readline()
+        if old_hash == hashed_password:
+            logger.log_event('Password matched.', __name__)
+            return True
+        logger.log_event('Password did not match.', __name__)
+        return False
+
+
 if __name__ == "__main__":
-    print(gen_or_get_salt())
+    password = input("Enter a password: ")
+    key = generate_key_from_password(password)
+    print(verify_password(password))
