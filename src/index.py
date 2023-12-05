3571bfad9e3be33965ead7d3f5b838ea09a528a9
NerosJourney <sandman4264@gmail.com>
2023-12-05 14:38:54 -0500

    Incorporated first window

--- src/index.py
+++ src/index.py
@@ -2,17 +2,30 @@ import logger
 logger.log_event('Started process', __name__)
 import db_manager
 import password_manager
+import gui
+import keygen
 
 success = False
+first_time = True
 while not success:
-    password = input("Enter your password: ")
-    success = password_manager.login(password)
-    print(success)
+    master = gui.show_login_window(not keygen.check_master_exists(), not first_time)
+    first_time = False
+    if not master:
+        logger.log_event('Exiting...', __name__)
+        exit()
+    success = password_manager.login(master)
 
-username = input('Enter username: ')
-# password = input('Enter password: ')
-url = 'http://google.com'
 
-# password_manager.add_entry(username, password, url)
+# success = False
+# while not success:
+#     password = input("Enter your password: ")
+#     success = password_manager.login(password)
+#     print(success)
 
-print(password_manager.get_password_for_entry(username, url))
+# username = input('Enter username: ')
+# # password = input('Enter password: ')
+# url = 'http://google.com'
+
+# # password_manager.add_entry(username, password, url)
+
+# print(password_manager.get_password_for_entry(username, url))
