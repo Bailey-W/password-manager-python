3507f09f95cf5f0bffb235fe51da7823920642c2
NerosJourney <sandman4264@gmail.com>
2023-12-05 11:30:09 -0500

    added logging and test code

--- src/index.py
+++ src/index.py
@@ -1,3 +1,6 @@
 import logger
-logger.log_event(f'Started process', __name__)
+logger.log_event('Started process', __name__)
 import db_manager
+import password_manager
+
+password_manager.add_entry("New User", "password", "www.spotify.com")
